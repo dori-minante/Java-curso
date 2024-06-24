7f38193b83794a1d8b513d85c074b079fd24095d
Dori Minante <dorim.dev@gmail.com>
2024-06-24 20:21:28 -0300

    first commit

---
+++ src/estrutura_sequencial/saidaDeDados.java
@@ -0,0 +1,9 @@
+package estrutura_sequencial;
+
+public class saidaDeDados {
+
+    public static void main(String[] args) {
+        System.out.print("Olá Mundo!");
+        System.out.println("Bom dia!");
+    }
+}
