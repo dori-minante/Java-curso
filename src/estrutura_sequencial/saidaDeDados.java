3cbe829086bb2e5ec1eab2149b7757b8e25cea4d
Dori Minante <dorim.dev@gmail.com>
2024-06-25 17:54:40 -0300

    exercicios

--- src/estrutura_sequencial/saidaDeDados.java
+++ src/estrutura_sequencial/saidaDeDados.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 public class saidaDeDados {
 
     public static void main(String[] args) {
-    //saída de dados
+        //saída de dados
         int y = 32;
         double x = 10.35784;
         System.out.println(y);
@@ -16,16 +16,16 @@ public class saidaDeDados {
 
         System.out.println("Bom dia!");
 
-    //concatenar vários elementos em um mesmo comando de escrita
+        //concatenar vários elementos em um mesmo comando de escrita
         System.out.println("RESULTADO = " + x + " METROS");
         System.out.printf("RESULTADO = %.2f metros%n", x);
 
-    //Exemplo:
-    // %f = ponto flutuante; %d = inteiro; %s = texto; %n = quebra de linha.
+        //Exemplo:
+        // %f = ponto flutuante; %d = inteiro; %s = texto; %n = quebra de linha.
 
-         String nome = "Maria";
-         int idade = 31;
-         double renda = 4000.0;
-         System.out.printf("%s tem %d anos e ganha R$ %.2f reais.%n", nome, idade, renda);
+        String nome = "Maria";
+        int idade = 31;
+        double renda = 4000.0;
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais.%n", nome, idade, renda);
     }
 }
